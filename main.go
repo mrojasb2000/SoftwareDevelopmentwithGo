49df8c54830b6069b8f3eb95f8c0494c5428d5d5
mauriciorojasb <mauricio.rojas@thoughtworks.com>
2023-04-28 17:22:22 -0400

    chore: using syscall golang standard library

--- main.go
+++ main.go
@@ -1,21 +1,19 @@
 package main
 
 import (
-	u "golang.org/x/sys/unix"
 	"log"
+	"syscall"
 )
 
 func main() {
-	c := make([]byte, 512)
+	log.Println("Getpid : ", syscall.Getpid())   // Obtains the process id of the current running app
+	log.Println("Getpgrp : ", syscall.Getpgrp()) // Obtains the group process id of the current running app
+	log.Println("Getppid : ", syscall.Getppid()) // Obtains the parent process id of the current running app
+	log.Println("Gettid : ", syscall.Getppid())  // Obtains the caller's thread it
 
-	log.Println("Getpid : ", u.Getpid())   // Obtains the process id of the current running app
-	log.Println("Getpgrp : ", u.Getpgrp()) // Obtains the group process id of the current running app
-	log.Println("Getppid : ", u.Getppid()) // Obtains the parent process id of the current running app
-	log.Println("Gettid : ", u.Getppid())  // Obtains the caller's thread it
-
-	_, err := u.Getcwd(c)
+	wd, err := syscall.Getwd()
 	if err != nil {
 		log.Fatalln(err)
 	}
-	log.Println(string(c))
+	log.Println(string(wd))
 }
